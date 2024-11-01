5db070ae57af2bba765957ec9dacc9e402f6c6e1
Keke Adya Mecca <kekeadyamecca@gmail.com>
2024-11-01 13:39:31 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "",
+"db_mhs"); 
+if ($mysqli->connect_error) {
+    die("Koneksi gagal: " . $mysqli->connect_error); 
+} else {
+    echo "Koneksi ke database berhasil.";
+}
